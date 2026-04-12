d93d611ca16d7bf405e0a52e8669ffad7f710e59
Daksh Verma <66217501+DakshVerma11@users.noreply.github.com>
2026-04-12 19:45:04 +0530

    Add: n-queens-ii - submission-2

---
+++ Data Structures & Algorithms/n-queens-ii/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        count=0
+        board = [[False] * n for i in range(n)]
+
+        def backtracking(r):
+            nonlocal count
+            if r == n:
+                count+=1
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = True
+
+                backtracking(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = False
+
+        backtracking(0)
+        return count
